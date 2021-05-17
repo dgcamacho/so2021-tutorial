9276a4941d8b5a0f98de72c70da69f7a7c35257c
Albrecht Michler <albrecht.michler@tu-dresden.de>
2021-05-17 15:03:41 +0200

    MInor fixes, added comments

--- exercise3/task4.cpp
+++ exercise3/task4.cpp
@@ -1,11 +1,11 @@
 #include <cassert>
 #include <iostream>
 
-double const tolerance = 1e-8;
+double const tolerance = 1e-12; // Value for absolute error in equality comparisons
 
 struct DualNumber {
-    double a;
-    double b;
+    double a; // real part
+    double b; // eps part
 
     DualNumber operator+(DualNumber dual) const{
         return DualNumber{this->a + dual.a, this->b + dual.b};
@@ -52,45 +52,45 @@ int main()
     DualNumber const zero{0, 0};
     DualNumber const one{1, 0};
     DualNumber const eps{0, 1};
-    auto const one_x_eps = one * eps;
 
     //Test assertions
     assert(one == one);
     assert(!(zero == one));
+    assert(eps * eps == zero); // Condition for dual numbers
 
     //Check identities
     //i)
-    assert(one_x_eps == eps * one);
-    assert(one_x_eps == eps);
+    assert(one * eps == eps * one);
+    assert(one * eps == eps);
     assert(eps * one == eps);
 
     //ii)
-    assert(one * one_x_eps == one_x_eps);
-    assert(one * one_x_eps == (one * one) * eps);
-    assert(one * one_x_eps == eps);
-    assert(one_x_eps == (one * one) * eps);
+    assert(one * (one * eps) == one * eps);
+    assert(one * (one * eps) == (one * one) * eps);
+    assert(one * (one * eps) == eps);
+    assert(one * eps == (one * one) * eps);
     assert((one * one) * eps == eps);
 
     //iii)
     assert(one * (eps * eps) == one * zero);
     assert(one * (eps * eps) == zero);
     assert(one * (eps * eps) == eps * eps);
-    assert(one * (eps * eps) == one_x_eps * eps);
+    assert(one * (eps * eps) == one * eps * eps);
     assert(one * zero == zero);
     assert(one * zero == eps * eps);
-    assert(one * zero == one_x_eps * eps);
+    assert(one * zero == one * eps * eps);
     assert(zero == eps * eps);
-    assert(zero == one_x_eps * eps);
-    assert(eps * eps == one_x_eps * eps);
+    assert(zero == (one * eps) * eps);
+    assert(eps * eps == (one * eps) * eps);
 
-    DualNumber const a{3, 1};
-    auto f_a = f(a);
-    auto f_prime_a = fPrime(a);
-    std::cout << "Original dual number a: ";
-    a.print();
-    std::cout << "Polynomial 5x^4 - 2x^2 + 7 evaluated with a. f(a)= ";
-    f_a.print();
-    std::cout << "Derivative of polynomial evaluated with a. f'(a)= ";
-    f_prime_a.print();
+    DualNumber const d{3, 1};
+    auto f_d = f(d);
+    auto f_prime_d = fPrime(d);
+    std::cout << "Original dual number d: ";
+    d.print();
+    std::cout << "Polynomial 5x^4 - 2x^2 + 7 evaluated with a. f(d)= ";
+    f_d.print();
+    std::cout << "Derivative of polynomial evaluated with a. f'(d)= ";
+    f_prime_d.print();
     return 0;
 }
