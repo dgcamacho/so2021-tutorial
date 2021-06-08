315b5c21b3dd55191ef9e944bbcda5f00c52409f
lotosbienchen <lukas.keller1@mailbox.tu-dresden.de>
2021-06-08 11:22:09 +0200

    finished necessary functions for crsmatrix

--- exercise5/material/CRSMatrix.hh
+++ exercise5/material/CRSMatrix.hh
@@ -1,8 +1,9 @@
-#pragma one
+#pragma once
 
 #include <cassert>
 #include <iostream>
 #include <vector>
+#include "Vector.hh"
 
 namespace scprog 
 {
@@ -31,7 +32,7 @@ namespace scprog
                                              indices_(nrows*nmax_nonzero),
                                              values_(nrows*nmax_nonzero), 
                                              offset_(nrows) {}
-
+            
         // class functions
         public:
             
@@ -47,27 +48,113 @@ namespace scprog
 
             void add(size_type i, size_type j, value_type value)
             {
+                std::cout << "Here comes the offset-check " << offset_[i] 
+                          << " Passed? " << (offset_[i] < nmax_nonzero_)
+                          << std::endl;
                 auto first = std::next(indices_.begin(), i*nmax_nonzero_);
                 auto last = std::next(first, offset_[i]);
-                //std::cout << first << " " << last << std::endl;
-                for (auto k = first; k < last; ++k){
-                    std::cout << indices_[k] << std::endl;
+
+                auto it = i*nmax_nonzero_ + binary_search(first, last, j);
+
+                if (j < indices_[it]) {
+                    shift(i);
+
+                    offset_[i] += 1;
+
+                    values_[it] = value_type(0);
+
+                } else if (values_[it] == value_type(0)) {
+                    offset_[i] += 1;
                 }
-                
+                indices_[it] = j;
+                values_[it] += value;
             }
 
-            void set(size_type i, size_type j, value_type value);
+            void set(size_type i, size_type j, value_type value)
+            {
+                std::cout << "Here comes the offset-check " << offset_[i] 
+                          << " Passed? " << (offset_[i] < nmax_nonzero_)
+                          << std::endl;
+                auto first = std::next(indices_.begin(), i*nmax_nonzero_);
+                auto last = std::next(first, offset_[i]);
 
-            void compress();
+                auto it = i*nmax_nonzero_ + binary_search(first, last, j);
 
-            void mv(Vector const &x, Vector& y);
+                if (j < indices_[it]) {
+                    shift(i);
+                    
+                    offset_[i] += 1;
+                } else if (values_[it] == value_type(0)) {
+                    offset_[i] += 1;
+                }
+                indices_[it] = j;
+                values_[it] = value;       
+            }
+
+            void compress()
+            {
+                                
+                for (size_type k = indices_.size(); k > 0; --k){
+                    if (indices_[k - 1] == size_type(0) && 
+                            values_[k - 1] == value_type(0)) {
+                        indices_.erase(indices_.begin() + k - 1);
+                        values_.erase(values_.begin() + k - 1);
+
+                    }
+                } 
+            }
+
+            void mv(Vector const &x, Vector& y)
+            {
+                assert(x.size() == cols_ && y.size() == rows_);
+                for (size_type i = 0; i < rows_; ++i) {
+                    if (offset_[i] == size_type(0)) {
+                        y[i] = value_type(0);
+                    } else {
+                        size_type lower_bound = offset_[i - 1];
+                        size_type upper_bound = lower_bound + offset_[i];
+                        
+                        for (size_type j = lower_bound; j < upper_bound; ++j) {
+                            y[i] += values_[j] * x[indices_[j]];
+                        }
+                    }
+                }
+            }
+
         private:
             size_type rows_;
             size_type cols_;
             size_type nmax_nonzero_;
-            std::vector<value_type> indices_;
+            std::vector<size_type> indices_;
             std::vector<value_type> values_;
-            std::vector<value_type> offset_;
+            std::vector<size_type> offset_;
+
+            auto binary_search(std::vector<size_type>::iterator first, 
+                               std::vector<size_type>::iterator last,
+                               size_type wanted) 
+            {
+                assert(last < indices_.end());
+                auto it = std::lower_bound(first, last, wanted);
+                return it - first;
+            
+            }
+
+            void shift(size_type i)
+            {
+                for (size_type k = (i+1)*nmax_nonzero_; 
+                        k > i*nmax_nonzero_ - 1; --k) {
+                    if (indices_[k] == size_type(0) && 
+                        values_[k] == value_type(0)) {
+                        if (indices_[k - 1] != size_type(0) && 
+                            values_[k - 1] != value_type(0)) {
+                            indices_[k] = indices_[k - 1];
+                            values_[k] = values_[k - 1];
+                            indices_[k - 1] = size_type(0);
+                            values_[k - 1] = value_type(0);
+                        }
+                    }
+                }
+            }
     };
 }
 
--- exercise5/material/task4.cc
+++ exercise5/material/task4.cc
@@ -1,9 +1,28 @@
 #include "CRSMatrix.hh"
+#include "Vector.hh"
 
 int main()
 {
-    scprog::CRSMatrix<double> A(3, 3, 3);
-    A.add(1, 2, 3);
+    std::vector<std::size_t> offset{2, 2, 2};
+    std::vector<std::size_t> indices{0, 2, 1, 2, 0, 1};
+    std::vector<double> values{1, 3, 2, -1, -4, -1};
+    scprog::CRSMatrix<double> A(3, 3, 3);//, indices, values, offset);
+    A.set(1, 1, 4);
+    A.add(1, 1, 3);
+    A.set(1, 2, 3);
+    A.add(1, 2, 5);
+    A.add(1, 0, 3);
+    A.add(2, 0, 2);
+
+    A.compress();
+
+    std::vector<double> const x_vec = {3,1,2};
+    scprog::Vector x(3);
+    x.fill(x_vec, 3);
+    scprog::Vector y(3);
+    std::cout << "Now we are multiplying" << std::endl;
+    A.mv(x, y);
+
 
     return 0;
 }
