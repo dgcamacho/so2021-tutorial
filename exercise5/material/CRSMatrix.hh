a49e21e6c69a89431f09a7b13a9c45c9ce28036f
lotosbienchen <lukas.keller1@mailbox.tu-dresden.de>
2021-06-08 12:02:02 +0200

    added comments in CRSMatrix

--- exercise5/material/CRSMatrix.hh
+++ exercise5/material/CRSMatrix.hh
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <cassert>
+#include <cassert>      
 #include <iostream>
 #include <vector>
 #include "Vector.hh"
@@ -35,65 +35,109 @@ namespace scprog
             
         // class functions
         public:
-            
+            // returns number of all rows, also 0-rows
             size_type rows() const 
             {
                 return rows_;
             }
 
+            // returns number of all columns, also 0-columns
             size_type cols() const
             {
                 return cols_;
             }
 
+            // add a value to a certain position in the matrix
             void add(size_type i, size_type j, value_type value)
             {
-                std::cout << "Here comes the offset-check " << offset_[i] 
-                          << " Passed? " << (offset_[i] < nmax_nonzero_)
-                          << std::endl;
+                
                 auto first = std::next(indices_.begin(), i*nmax_nonzero_);
                 auto last = std::next(first, offset_[i]);
 
+                //index of the position where value shall be added
                 auto it = i*nmax_nonzero_ + binary_search(first, last, j);
 
+                // if the index stored at the wanted position is larger than 
+                // the one to be added, the stored index has to be shifted to 
+                // the right
                 if (j < indices_[it]) {
-                    shift(i);
+                    // if the max no. of non-zero entries is already reached, 
+                    // the shift is not allowed! Otw. shift and increase value
+                    // of offset_ at row i AND reset value at wanted index
+                    if (offset_test(i)) {
+                        shift(i);
 
-                    offset_[i] += 1;
+                        offset_[i] += 1;
 
-                    values_[it] = value_type(0);
+                        values_[it] = value_type(0);
+                    } else {
+                        std::cout << "This row already contains max no. of "
+                                  << "non-zero entries!" << std::endl;
+                        abort();
+                    }
 
                 } else if (values_[it] == value_type(0)) {
-                    offset_[i] += 1;
+                    // if value at wanted index is zero (no entry set or added)
+                    // and max no. of non-zero entries is not reached, increase
+                    // offset_ at row i.
+                    if (offset_test(i)) {
+                        offset_[i] += 1;
+                    } else {
+                        std::cout << "This row already contains max no. of "
+                                  << "non-zero entries!" << std::endl;
+                        abort();
+                    }
                 }
                 indices_[it] = j;
                 values_[it] += value;
             }
 
+            // set a certain entry of the matrix to the passed value
             void set(size_type i, size_type j, value_type value)
             {
-                std::cout << "Here comes the offset-check " << offset_[i] 
-                          << " Passed? " << (offset_[i] < nmax_nonzero_)
-                          << std::endl;
                 auto first = std::next(indices_.begin(), i*nmax_nonzero_);
                 auto last = std::next(first, offset_[i]);
 
+                // index of the position where a value shall be set
                 auto it = i*nmax_nonzero_ + binary_search(first, last, j);
 
+                // if the index stored at the wanted position is larger than 
+                // the one to be inserted, the stored index has to be shifted
+                // to the right
                 if (j < indices_[it]) {
-                    shift(i);
+                    // if the max no. of non-zero entries is already reached,
+                    // the shift is not allowed! Otw. shift and increase value
+                    // of offset_ at row i
+                    if (offset_test(i)) {
+                        shift(i);
                     
-                    offset_[i] += 1;
+                        offset_[i] += 1;
+                    } else {
+                        std::cout << "This row already contains max no. of "
+                                  << "non-zero entries!" << std::endl;
+                        abort();
+                    }
                 } else if (values_[it] == value_type(0)) {
-                    offset_[i] += 1;
+                    // if value at wanted index is zero (no entry set or added)
+                    // and max no. of non-zero entries is not reached, increase
+                    // offset_ at row i.
+                    if (offset_test(i)) {
+                        offset_[i] += 1;
+                    } else {
+                        std::cout << "This row already contains max no. of "
+                                  << "non-zero entries!" << std::endl;
+                        abort();                       
+                    }
                 }
                 indices_[it] = j;
                 values_[it] = value;       
             }
 
+            // delete all zero-entries from the indices_ and values_ vectors
             void compress()
             {
-                                
+                // iterate reversively over all elements to not get size prob-
+                // lems because of deleted entries
                 for (size_type k = indices_.size(); k > 0; --k){
                     if (indices_[k - 1] == size_type(0) && 
                             values_[k - 1] == value_type(0)) {
@@ -104,10 +148,14 @@ namespace scprog
                 } 
             }
 
+            // calculate y = A*x
             void mv(Vector const &x, Vector& y)
             {
+                // check for dimensions
                 assert(x.size() == cols_ && y.size() == rows_);
                 for (size_type i = 0; i < rows_; ++i) {
+                    // if the row does not contain non-zero entries, the y-val
+                    // is zero
                     if (offset_[i] == size_type(0)) {
                         y[i] = value_type(0);
                     } else {
@@ -122,38 +170,59 @@ namespace scprog
             }
 
         private:
-            size_type rows_;
-            size_type cols_;
-            size_type nmax_nonzero_;
-            std::vector<size_type> indices_;
-            std::vector<value_type> values_;
-            std::vector<size_type> offset_;
+            size_type rows_;                 // number of all rows
+            size_type cols_;                 // number of all cols
+            size_type nmax_nonzero_;         // number of max. non-zero entries
+                                             // per row
+            std::vector<size_type> indices_; // indices where values != 0
+            std::vector<value_type> values_; // non-zero values
+            std::vector<size_type> offset_;  // no. of non-zero entries per row
 
+            // searches for the first element in a range of elements that is
+            // not less than the wanted type.
             auto binary_search(std::vector<size_type>::iterator first, 
                                std::vector<size_type>::iterator last,
                                size_type wanted) 
             {
                 assert(last < indices_.end());
                 auto it = std::lower_bound(first, last, wanted);
+
+                // returning the distance of the first pointer and the pointer
+                // corresponding to the wanted value, which is usable as 
+                // size_type
                 return it - first;
             
             }
 
+            // shift elements of a given row to a neighbouring non-zero entry
             void shift(size_type i)
             {
+                // iterate reversively over all column-elements in the given 
+                // row
                 for (size_type k = (i+1)*nmax_nonzero_; 
                         k > i*nmax_nonzero_ - 1; --k) {
+                    // if current entry is zero-entry, shift the left entry
+                    // into current entry
                     if (indices_[k] == size_type(0) && 
                         values_[k] == value_type(0)) {
                         if (indices_[k - 1] != size_type(0) && 
                             values_[k - 1] != value_type(0)) {
                             indices_[k] = indices_[k - 1];
                             values_[k] = values_[k - 1];
+
+                            // clear left entries, so that next-to-left entry
+                            // can be shifted again
                             indices_[k - 1] = size_type(0);
                             values_[k - 1] = value_type(0);
                         }
                     }
                 }
+            }
+
+            // checks whether the maximun no. of non-zero entries is reached
+            bool offset_test(size_type i) 
+            {
+                return (offset_[i] < nmax_nonzero_) ? 1 : 0;
             }
     };
 }
