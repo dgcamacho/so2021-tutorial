7d4e2ecf4b4eba08b4c554f2f27e0c779cdbef61
Tony Zorman <tony.zorman@tu-dresden.de>
2021-06-09 15:10:56 +0200

    task4.cc: Add task 4 on sheet 6
    In addition to the already existing one, add another simple test case.
    
    This also adds some basic comparisons with the exact results (thank you,
    WolframAlpha) to both examples and hence changes out the types for
    floats.

--- exercise5/task4.cc
+++ exercise5/task4.cc
@@ -1,5 +1,6 @@
 #include "crs-matrix.hh"
 
+#include <cmath>
 #include <iostream>
 
 using namespace scprog;
@@ -15,11 +16,21 @@ auto operator<<(OStream& os, Vector<T> const& vec) -> OStream& {
   return os << vec[vec.size() - 1] << "]";
 }
 
+template <typename T>
+auto two_norm(Vector<T> const& l, Vector<T> const& r) -> T {
+  assert(l.size() == r.size());
+  T res{0};
+  for (decltype(l.size()) i = 0; i < l.size(); ++i) {
+    res += l[i] * l[i] - r[i] * r[i];
+  }
+  return std::sqrt(res);
+}
+
 auto main() -> int {
   // Example 1
-  CRSMatrix<int> mat{4, 4, 4};
-  Vector<int> y{1, 2, 3, 4};
-  Vector<int> x(y.size());
+  CRSMatrix<double> mat{4, 4, 4};
+  Vector<double> y{1, 2, 3, 4};
+  Vector<double> x(y.size());
 
   // Fill matrix
   mat.set(0, 0, 1);
@@ -33,7 +44,31 @@ auto main() -> int {
   mat.set(3, 3, 3);
 
   // Compress and multiply
-  CRSMatrix<int>::CompressedMatrix com = mat.compress();
+  CRSMatrix<double>::CompressedMatrix com = mat.compress();
   com.mv(y, x);
-  std::cout << x << std::endl;
+  Vector<double> exact{10, 0, -3, 13};
+  std::cout << "Example 1 exact result: " << exact              << '\n'
+            << "Computed result: "        << x                  << '\n'
+            << "2-norm difference: "      << two_norm(exact, x) << "\n\n";
+
+  // Example2: task 4 on sheet 6
+  CRSMatrix<double> mat2{3, 3, 3};
+  Vector<double> y2{1, 1, 1};
+  Vector<double> x2(y2.size());
+
+  // Insert from the back to test that
+  mat2.set(0, 1, -1);
+  mat2.set(0, 0, 2);
+  mat2.set(1, 2, -1);
+  mat2.set(1, 1, 2);
+  mat2.set(1, 0, -1);
+  mat2.set(2, 2, 2);
+  mat2.set(2, 1, -1);
+
+  CRSMatrix<double>::CompressedMatrix com2 = mat2.compress();
+  com2.mv(y2, x2);
+  Vector<double> exact2{1, 0, 1};
+  std::cout << "Example 2 exact result: " << exact2               << '\n'
+            << "Computed result: "        << x2                   << '\n'
+            << "2-norm difference: "      << two_norm(exact2, x2) << '\n';
 }
