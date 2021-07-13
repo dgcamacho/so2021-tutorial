9823d2e0cd285d4aa19118a551cdba414e4bd9d3
Tony Zorman <tony.zorman@tu-dresden.de>
2021-07-13 18:00:39 +0200

    Add simple expression template skeleton

---
+++ exercise10/ExpressionTemplates.hh
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <utility>
+
+namespace scprog {
+
+// An arbitrary binary expression, acting on types at a certain index.
+template <typename T, typename S, std::invocable<T, S, std::size_t> F>
+class BinaryExpr {
+private:
+  T const& _a;
+  S const& _b;
+  F        _f;
+
+public:
+  using value_type = std::result_of<F(T, S, std::size_t)>::type;
+
+  BinaryExpr(T const& a, S const& b, F f)
+      : _a(a)
+      , _b(b)
+      , _f(f) {}
+
+  // Get the result of the supplied function at the inde `i`.
+  auto operator[](std::size_t i) const -> value_type { return _f(_a, _b, i); }
+
+  auto size() const -> std::size_t { return _a.size(); }
+};
+
+template <typename T, typename S>
+auto operator+(T const& a, S const& b) {
+  return BinaryExpr{a, b, [](auto const& x, auto const& y, std::size_t i) {
+                      return x[i] + y[i];
+                    }};
+}
+
+template <typename T, typename S>
+auto operator-(T const& a, S const& b) {
+  return BinaryExpr{a, b, [](auto const& x, auto const& y, std::size_t i) {
+                      return x[i] - y[i];
+                    }};
+}
+
+} // namespace scprog
